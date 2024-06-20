3e5b9d22d98b8f1d99f7e8761c74c96ad4be2d73
ic3K1ng <emuandula69@gmail.com>
2024-06-20 03:49:54 +0100

    Class para perguntas e respostas

---
+++ app/src/main/java/com/example/myproject/Perguntas.kt
@@ -0,0 +1,10 @@
+package com.example.myproject
+
+data class Perguntas(
+    val question: String,
+    val answer1: String,
+    val answer2: String,
+    val answer3: String,
+    val answer4: String,
+    val correctAnswer: String
+)
